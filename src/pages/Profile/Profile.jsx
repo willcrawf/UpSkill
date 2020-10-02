3321d011777e0b29bfe4bbbf1d1f17f62b366c72
Will Crawford <will@Wills-MacBook-Pro.local>
2020-10-01 21:02:14 -0400

    add some tutor verification

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import authService from "../../services/authService";
 
 class Profile extends Component {
     state = {
-        
+        user: authService.getUser(),
       }
     render() { 
         return (
             <>
-            <h1>Profile Page</h1>
+            <h1>Welcome, {this.state.user.name}</h1>
+            <h1>Are you a tutor? {this.state.user.isTutor === true ?  "Yes" : "No"} </h1>
+
+
             </>
           );
     }
