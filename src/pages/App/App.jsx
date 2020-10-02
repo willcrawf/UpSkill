bff9fd3224cb6dcae810d44c418080b9f355b2d5
Will Crawford <will@Wills-MacBook-Pro.local>
2020-10-01 21:11:06 -0400

    add MENU on profile page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 import Signup from "../Signup/Signup";
+import 'semantic-ui-css/semantic.min.css'
 import Login from "../Login/Login";
 import authService from "../../services/authService";
 import Users from "../Users/Users";
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import authService from "../../services/authService";
-
+import MenuEl from "../../components/Menu/Menu"
 class Profile extends Component {
     state = {
         user: authService.getUser(),
@@ -9,9 +9,9 @@ class Profile extends Component {
         return (
             <>
             <h1>Welcome, {this.state.user.name}</h1>
-            <h1>Are you a tutor? {this.state.user.isTutor === true ?  "Yes" : "No"} </h1>
-
-
+            <h1>Are you a tutor? {this.state.user.isTutor ?  "Yes" : "No"} </h1>
+            
+            < MenuEl />
             </>
           );
     }
